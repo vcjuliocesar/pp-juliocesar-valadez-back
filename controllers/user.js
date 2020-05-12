90a028350595deb7b01da778ec86586050f3ae15
julio cesar <vcjuliocesar@outlook.com>
2020-05-12 11:10:10 -0500

    save user

--- controllers/user.js
+++ controllers/user.js
@@ -1,50 +1,78 @@
 'use strict'
 
 var validator = require('validator');
-//var User = require('');
+var moment = require('moment');
+var User = require('../models/user');
 
 var controller = {
-    
-    probando:function(req,res){
-        return res.status(200).send({
-            message : "Soy el metodo porbando"
-        });
-    },
 
-    testeando:function(req,res){
-        return res.status(200).send({
-            message:"Soy el mentodo testeando"
-        });
-    },
-
-    save: function(req,res){
+    save: function (req, res) {
         //recoger parametros de la peticion
         var params = req.body;
         //validar datos
         var validate_name = !validator.isEmpty(params.name),
-            validate_phone = !validator.isEmpty(params.phone) && validator.isMobilePhone(params.phone,['es-MX']),
-            validate_age = !validator.isEmpty(params.age) && validator.isInt(params.age,{gt:17,lt:99}),
-            validate_gender = !validator.isEmpty(params.gender) && validator.isIn(params.gender,["male", "female"]);
+            validate_phone = !validator.isEmpty(params.phone) && validator.isMobilePhone(params.phone, ['es-MX']),
+            validate_age = !validator.isEmpty(params.age) && validator.isInt(params.age, {
+                gt: 17,
+                lt: 99
+            }),
+            validate_gender = !validator.isEmpty(params.gender) && validator.isIn(params.gender.toLowerCase(), ["male", "female"]);
 
-            if(validate_name && validate_phone && validate_age && validate_gender) {
-                //crear objeto
-                //asignar valores al objeto
-                //comprobar si el usuario existe
-                //guardarlo
-                //respuesta
-            } else {
-                return res.status(200).send({
-                    message: "Error",
-                });
-            }
+        if (validate_name && validate_phone && validate_age && validate_gender) {
+            //crear objeto
+            var user = new User();
 
-       
+            //asignar valores al objeto
+            user.name = params.name;
+            user.phone = params.phone;
+            user.age = params.age;
+            user.gender = params.gender;
+            user.created_at = moment().format();
+        
+            //comprobar si el usuario existe
+            User.findOne({
+                name: user.name
+            }, (err, issetUser) => {
+                if (err) {
+                    return res.status(500).send({
+                        message: "Error al comprobar duplicidad de usuario",
+                    });
+                }
 
-        return res.status(200).send({
-            message: "Registro de usuarios",
-            params
-        });
-    }
+                if (!issetUser) {
+                    //guardarlo
+                    user.save((err, userStored) => {
+                        if(err) {
+                            return res.status(500).send({
+                                message: "Error al guardar el usuario"
+                            });
+                        }
+
+                        if(!userStored){
+                            return res.status(400).send({
+                                message:"El usuario no se guardo"
+                            });
+                        }
+                        //respuesta
+                        return res.status(200).send({
+                            message:"success",
+                            user:userStored
+                        });
+                    });
+            
+                } else {
+                    return res.status(200).send({
+                        message: "El usuario ya esta registrado",
+                    });
+                }
+            });
+
+        } else {
+            return res.status(200).send({
+                message: "Validación de los datos del usuario incorrecta",
+            });
+        }
+    },
 };
 
 module.exports = controller;
--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,6 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var router = express.Router();
-//Rutas de prueba borralas
-router.get('/probando',UserController.probando);
-router.post('/testeando',UserController.testeando);
 
 //Rutas de usuario
 router.post('/register',UserController.save);
