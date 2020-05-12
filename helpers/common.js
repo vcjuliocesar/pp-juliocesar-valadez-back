f891b40e0ad708ea7bdcc699690af835f0dcc34b
julio cesar <vcjuliocesar@outlook.com>
2020-05-12 18:31:27 -0500

    fix capital_letter method

--- helpers/common.js
+++ helpers/common.js
@@ -2,7 +2,7 @@
 
 var common = {
     capital_letter: function (req, res) {
-        var str = req.split(" ");
+        var str = req.trim().split(" ");
 
         for (var i = 0; i < str.length; i++) {
             str[i] = str[i][0].toUpperCase() + str[i].substr(1);
