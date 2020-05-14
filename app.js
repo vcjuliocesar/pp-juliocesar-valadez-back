42d7fca3f8f2cf6aaaa0f5cd11c669c56acfed1e
julio cesar <vcjuliocesar@outlook.com>
2020-05-13 20:55:32 -0500

    fix cross CORS

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var user_routes = require('./routes/user');
 // Middlewares
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Cors
 
